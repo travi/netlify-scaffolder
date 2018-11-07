a3ce1005e5b3aa5e35eeae68413be7c711659318
Matt Travi <programmer@travi.org>
2018-11-06 23:08:08 -0600

    feat(config): defined initial details

---
+++ src/scaffolder.js
@@ -0,0 +1,9 @@
+export default function ({buildDirectory}) {
+  return {
+    scripts: {
+      deploy: `netlify deploy --site=$NETLIFY_SITE_ID --auth=$NETLIFY_ACCESS_TOKEN --dir=./${buildDirectory}/ --prod`
+    },
+    environmentVariables: {ci: ['NETLIFY_SITE_ID', 'NETLIFY_ACCESS_TOKEN']},
+    devDependencies: ['netlify-cli']
+  };
+}
---
+++ test/unit/scaffolder-test.js
@@ -0,0 +1,23 @@
+import {assert} from 'chai';
+import any from '@travi/any';
+import {scaffold} from '../../src';
+
+suite('scaffolder', () => {
+  const buildDirectory = any.string();
+
+  test('that the deploy script is defined', async () => {
+    const results = await scaffold({buildDirectory});
+
+    assert.equal(
+      results.scripts.deploy,
+      `netlify deploy --site=$NETLIFY_SITE_ID --auth=$NETLIFY_ACCESS_TOKEN --dir=./${buildDirectory}/ --prod`
+    );
+    assert.deepEqual(results.devDependencies, ['netlify-cli']);
+  });
+
+  test('that the required ci environment variables are listed', async () => {
+    const results = await scaffold({buildDirectory});
+
+    assert.deepEqual(results.environmentVariables.ci, ['NETLIFY_SITE_ID', 'NETLIFY_ACCESS_TOKEN']);
+  });
+});
