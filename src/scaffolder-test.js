f48e7870630e74398cbd273dfb675864998eeccf
Matt Travi <programmer@travi.org>
2020-10-25 16:21:32 -0500

    feat(config): generated an intial netlify config file that sets default security headers

--- src/scaffolder-test.js
+++ src/scaffolder-test.js
@@ -1,9 +1,21 @@
+import {promises as fs} from 'fs';
+import {resolve} from 'path';
 import {assert} from 'chai';
 import any from '@travi/any';
+import sinon from 'sinon';
 import {scaffold} from './index';
 
 suite('scaffolder', () => {
+  let sandbox;
   const buildDirectory = any.string();
+
+  setup(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(fs, 'copyFile');
+  });
+
+  teardown(() => sandbox.restore());
 
   test('that the deploy script is defined', async () => {
     const results = await scaffold({buildDirectory});
@@ -21,5 +33,17 @@ suite('scaffolder', () => {
     const results = await scaffold({buildDirectory});
 
     assert.deepEqual(results.vcsIgnore.directories, ['/.netlify/']);
+  });
+
+  test('that the netlify config file is created', async () => {
+    const projectRoot = any.string();
+
+    await scaffold({buildDirectory, projectRoot});
+
+    assert.calledWith(
+      fs.copyFile,
+      resolve(__dirname, '..', 'templates', 'netlify.toml'),
+      `${projectRoot}/netlify.toml`
+    );
   });
 });
--- src/scaffolder.js
+++ src/scaffolder.js
@@ -1,4 +1,9 @@
-export default function ({buildDirectory}) {
+import {promises as fs} from 'fs';
+import {resolve} from 'path';
+
+export default async function ({buildDirectory, projectRoot}) {
+  await fs.copyFile(resolve(__dirname, '..', 'templates', 'netlify.toml'), `${projectRoot}/netlify.toml`);
+
   return {
     scripts: {
       'predeploy:netlify': 'run-s build',
