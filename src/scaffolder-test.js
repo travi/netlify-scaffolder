32e96a56fd823c1de4132073530ceba6dd9789fd
Matt Travi <programmer@travi.org>
2020-10-25 17:42:53 -0500

    feat(next-steps): documented steps that are not handled by the scaffolder

--- src/scaffolder-test.js
+++ src/scaffolder-test.js
@@ -46,4 +46,32 @@ suite('scaffolder', () => {
       `${projectRoot}/netlify.toml`
     );
   });
+
+  test('that next-steps are listed for steps not completed by the scaffolder', async () => {
+    const results = await scaffold({buildDirectory});
+
+    assert.deepEqual(
+      results.nextSteps,
+      [
+        {
+          summary: 'Create a new site through the Netlify web UI',
+          description: `- [ ] Set the site name to something more description than the auto-generated one
+- [ ] Configure a custom domain`
+        },
+        {summary: 'Make `NETLIFY_AUTH_TOKEN` available to the deploy step on CI at an account level'},
+        {summary: 'Make `NETLIFY_SITE_ID` available to the deploy step on CI at a repository level'},
+        {
+          summary: 'Redirect the netlify url to the custom url in the `netlify.toml`',
+          description: `
+\`\`\`toml
+[[redirects]]
+from = "https://pragmatic-divops.netlify.com/*"
+to = "https://pragmatic-divops.dev/:splat"
+force = true
+\`\`\`
+`
+        }
+      ]
+    );
+  });
 });
--- src/scaffolder.js
+++ src/scaffolder.js
@@ -9,6 +9,26 @@ export default async function ({buildDirectory, projectRoot}) {
       'predeploy:netlify': 'run-s build',
       'deploy:netlify': `npx --package netlify-cli netlify deploy --dir=${buildDirectory}/`
     },
-    vcsIgnore: {directories: ['/.netlify/']}
+    vcsIgnore: {directories: ['/.netlify/']},
+    nextSteps: [
+      {
+        summary: 'Create a new site through the Netlify web UI',
+        description: `- [ ] Set the site name to something more description than the auto-generated one
+- [ ] Configure a custom domain`
+      },
+      {summary: 'Make `NETLIFY_AUTH_TOKEN` available to the deploy step on CI at an account level'},
+      {summary: 'Make `NETLIFY_SITE_ID` available to the deploy step on CI at a repository level'},
+      {
+        summary: 'Redirect the netlify url to the custom url in the `netlify.toml`',
+        description: `
+\`\`\`toml
+[[redirects]]
+from = "https://pragmatic-divops.netlify.com/*"
+to = "https://pragmatic-divops.dev/:splat"
+force = true
+\`\`\`
+`
+      }
+    ]
   };
 }
